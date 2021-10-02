e12a679f5354b19c2380b725a269f6768777da49
Julius Lee <7julius.lee@gmail.com>
2021-10-02 02:05:40 -0700

    added packages for animation, icons, and router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import HeaderNav from './components/HeaderNav';
 import Hero from './components/Hero/index';
+import About from './components/About/index';
 import Project from './components/Project/index';
+import Contact from './components/Contact/index';
+import Footer from './components/Footer/index';
 
 import './App.scss';
+import { useRef } from 'react';
 
 function App() {
+    const aboutRef = useRef();
     return (
-        <div class="app">
+        <div className="app">
+            <Hero aboutRef={aboutRef} />
             <HeaderNav />
-            <Hero />
+            <About ref={aboutRef} />
             <Project />
+            <Contact />
+            <Footer />
         </div>
     );
 }
