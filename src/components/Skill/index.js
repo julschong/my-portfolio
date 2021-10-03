5343337ebd26bedf6d578c38a1c90c6a2db0691b
Julius Lee <7julius.lee@gmail.com>
2021-10-02 17:38:28 -0700

    added init skill subsection

---
+++ src/components/Skill/index.js
@@ -0,0 +1,14 @@
+const Skill = () => {
+    return (
+        <div>
+            <div>
+                <div>Skill1</div>
+                <div>Skill2</div>
+                <div>Skill3</div>
+                <div>Skill4</div>
+            </div>
+        </div>
+    );
+};
+
+export default Skill;
