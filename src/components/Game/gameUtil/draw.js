a7664291d62fed232852cabce14f0930051a7eb2
Julius Lee <7julius.lee@gmail.com>
2021-10-02 17:40:06 -0700

    function update to take scroll position into account

--- src/components/Game/gameUtil/draw.js
+++ src/components/Game/gameUtil/draw.js
@@ -1,7 +1,13 @@
-export const draw = (ctx, frameCount, x, y) => {
+export const draw = (ctx, frameCount, x, y, scrollY) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = '#FFFFFF';
     ctx.beginPath();
-    ctx.arc(x, y, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.arc(
+        x,
+        y + scrollY,
+        20 * Math.sin(frameCount * 0.05) ** 2,
+        0,
+        2 * Math.PI
+    );
     ctx.fill();
 };
