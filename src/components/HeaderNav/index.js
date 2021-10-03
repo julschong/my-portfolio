69351780eff44ffc41f1af5b0e1c13b22a71ec73
Julius Lee <7julius.lee@gmail.com>
2021-10-02 17:30:54 -0700

    added fixed nav

--- src/components/HeaderNav/index.js
+++ src/components/HeaderNav/index.js
@@ -1,17 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import './HeaderNav.scss';
 
-const HeaderNav = () => {
-    // const [scrollPosition, setScrollPostion] = useState();
+const HeaderNav = (props) => {
+    const navRef = useRef();
 
-    // useEffect(() => {
-    //     window.addEventListener('scroll', (e) => {
-    //         console.log(e);
-    //     });
-    // }, []);
+    if (navRef.current && window.scrollY > navRef.current.offsetTop) {
+        return (
+            <>
+                <div className="nav-space-holder" ref={navRef}></div>
+                <nav
+                    className="nav"
+                    style={{
+                        position: 'fixed'
+                    }}
+                >
+                    <ul>
+                        <li>
+                            <a href="#about">ABOUT</a>
+                        </li>
+                        <li>
+                            <a href="#my-work">PROJECT</a>
+                        </li>
+                        <li>
+                            <a href="#contact">CONTACT</a>
+                        </li>
+                    </ul>
+                </nav>
+            </>
+        );
+    }
 
     return (
-        <nav className="nav">
+        <nav className="nav" ref={navRef}>
             <ul>
                 <li>
                     <a href="#about">ABOUT</a>
