8f5471f37a64b11551646e55b268ec76e43c2f73
Julius Lee <7julius.lee@gmail.com>
2021-10-06 08:19:36 -0700

    fixed more typo, minor change to about me

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -47,7 +47,7 @@ const About = forwardRef((props, ref) => {
                     <p className="profile-caption">
                         My name is Julius Lee.
                         <br />
-                        I'm an self-motivated, aspiring Fullstack Developer.
+                        I'm a self-motivated, aspiring Fullstack Developer.
                     </p>
                 </div>
                 <p
@@ -60,9 +60,9 @@ const About = forwardRef((props, ref) => {
                 >
                     Hi there!
                     <br />I graduated from University of California, Riverside
-                    with Bachelor of Science in Electrical Engineering in 2015.
-                    That's where I first learned programming with C++ and C, and
-                    I also worked on quite a bit of Micro-controller
+                    with a Bachelor of Science in Electrical Engineering in
+                    2015. That's where I first learned programming with C++ and
+                    C, and I also worked on quite a bit of Micro-controller
                     Programming. With my degree, I began my career as a
                     Procurement Engineer, but in 2019 I had a chance to use SQL
                     at work and fell in love with solving complex problems with
